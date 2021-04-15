27b0a9a3f5169674766952a6cda13dac3431f23d
vijaytripathi553 <68139428+vijaytripathi553@users.noreply.github.com>
2021-04-15 11:39:00 +0530

    Create DisplayPositiveElements.java

---
+++ DisplayPositiveElements.java
@@ -0,0 +1,22 @@
+package com.array_interview.programs;
+
+public class DisplayPositiveElements {
+	private static int [] findPositiveElements(int array1[])
+	{
+		for (int i = 0; i < array1.length; i++) {
+			if(array1[i]>0)
+			{
+				System.out.print(array1[i]+" ");
+			}
+			
+		}
+		return array1;
+	}
+	//Main Method
+	public static void main(String args[])
+	{
+		int array1[]= {20,-56,45,3,-78,7,-1};
+		DisplayPositiveElements.findPositiveElements(array1);
+	}
+
+}
