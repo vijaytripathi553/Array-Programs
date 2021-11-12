a6ee6420f6ed88c3aa23938f67e6f7e79c77b72d
Vijay Tripathi <68139428+vijaytripathi553@users.noreply.github.com>
2021-11-12 23:42:25 +0530

    Update AjayArrayChallenge1.java

--- AjayArrayChallenge1.java
+++ AjayArrayChallenge1.java
@@ -1,5 +1,25 @@
 package com.live.interview;
 
+
+ *HAve the function ArrayChallenge(arr) take the array of numbers stored in arr and from the position in array where a 1 is, return the number of spacecs either left or right.
+ * You must move to reach an enemy which is represented by a 2.
+ * 
+ * For example: If arr is [0,0,1,0,0,2,0,2] then your program should return 3 because the closest enemy (2) is 3 spaces away from the 1. The array will contain any number of 0's and 2's, but only a single 1. 
+ * It may nt contain any 2's at all as well, where in that case your program should return a 0;
+ * Example\
+ * Input: new int[] {1,1,1,1,2,2,2}
+ * Output: 4
+ * 
+ * 
+ * Input: new int[] {2,0,0,0,2,2,1,0}
+ * Output: 1
+	 
+	 
+	 
+	 
+	 
+	 
+
 import java.util.Iterator;
 
 public class AjayArrayChallenge1 
